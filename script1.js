361384e9c8340fa333522b8d11bb33a1e17b7727
anastasia-dev-007 <133599958+anastasia-dev-007@users.noreply.github.com>
2023-07-26 18:09:41 +0300

    first commit

---
+++ script1.js
@@ -0,0 +1,13 @@
+// Se dă un array cu numere. Scrie un algoritm care va elimina duplicatele din acest array.
+
+const numbers = new Set();
+
+
+numbers.add(15);
+numbers.add(20);
+numbers.add(15);
+numbers.add(20);
+numbers.add(15);
+numbers.add(20);
+
+console.log(numbers);
