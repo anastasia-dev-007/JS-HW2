5c0053f4e2a6724ab6761833e550a35966d90699
anastasia-dev-007 <133599958+anastasia-dev-007@users.noreply.github.com>
2023-07-28 08:52:54 +0300

    task 3 in progress

---
+++ script3.js
@@ -0,0 +1,18 @@
+// De creat un program care simulează activitatea unui depozit. Produsele pot avea următoarele proprietăți(și multe altele după preferință):
+// name: string,
+// category: string,
+// price: number,
+// originCountry: { name: string, code: string },
+// etc….
+
+// Scenariu:
+// Exista o listă de produse.
+// Posibilitatea de a suplini stocul produselor.
+// Posibilitatea de a lua produse din depozit.
+// De grupat produsele după categorie.
+// Posibilitatea de a returna produsele la depozit.
+// De grupat produsele după țara de origine.
+// Să se găsească produsul cel mai scump.
+// Să se găsească produsul cel mai ieftin.
+// Să se găsească produsele cu prețul între 600 și 1000.
+// etc….
